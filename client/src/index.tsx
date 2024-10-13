0c9d7d87532092f53b39e2f3182ca5793134b7c2
shreve <eric@shreve.dev>
2024-10-13 14:00:45 -0700

    refactor: reorganize project layout

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initClientInstrumentation } from './otel';
+initClientInstrumentation();
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
